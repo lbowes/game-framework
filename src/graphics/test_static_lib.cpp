291843cedfd18b6c3f8b948f2e5d807213697815
Lewis Bowes <33807016+lbowes@users.noreply.github.com>
2023-02-14 00:49:43 +0000

    Rename root directory from `src` -> `game-framework`

--- src/graphics/test_static_lib.cpp
+++
@@ -1,7 +0,0 @@
-#include "include/Logger.h"
-
-int main() {
-    const std::string& msg = "Here is some test info to be logged";
-    GF::Utils::Logger::log(GF::Utils::Logger::LogType::INFO, msg);
-    return 0;
-}
